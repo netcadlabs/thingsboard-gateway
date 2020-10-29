8a141815f734fcd8c9793b3e313a0b9f89ecf4da
agtokty <agitoktay@gmail.com>
2020-10-29 13:33:08 +0300

    update camera connector

--- thingsboard_gateway/connectors/camera/ndugate_camera_connector.py
+++ thingsboard_gateway/connectors/camera/ndugate_camera_connector.py
@@ -4,27 +4,26 @@ NDU-Gate
 connector ayar dosyası : etc/thingsboard-gateway/config/camera.json
 """
 
-import sys
 import time
 from threading import Thread
 from random import choice
 from string import ascii_lowercase
-import datetime
-from datetime import datetime, date
-from datetime import timezone, timedelta
-import requests
 import json
-import re
-import subprocess
-from os import path
 import zmq
 
 from thingsboard_gateway.connectors.connector import Connector, log
-from thingsboard_gateway.tb_utility.tb_utility import TBUtility
 
 # TODO - use config
 HOSTNAME = "127.0.0.1"
 PORT = "60600"
+
+
+def log_exception(e):
+    if hasattr(e, 'message'):
+        log.error(e.message)
+    else:
+        log.exception(e)
+
 
 class NDUGateCameraConnector(Thread, Connector):
     def __init__(self, gateway, config, connector_type):
@@ -36,10 +35,10 @@ class NDUGateCameraConnector(Thread, Connector):
         # get from the configuration or create name for logs.
         self.setName(self.__config.get("name", "Custom %s connector " % self.get_name() + ''.join(choice(ascii_lowercase) for _ in range(5))))
         log.info("Starting Custom %s connector", self.get_name())
-        
-        self.daemon = True    # Set self thread as daemon
-        self.stopped = True    # Service variable for check state
-        self.__connected = False    # Service variable for check connection to device
+
+        self.daemon = True  # Set self thread as daemon
+        self.stopped = True  # Service variable for check state
+        self.__connected = False  # Service variable for check connection to device
         self.__devices = {}
         self.__masterCameraName = self.__config.get("devices")[0].get("name")
 
@@ -53,12 +52,12 @@ class NDUGateCameraConnector(Thread, Connector):
             self.deviceLastAttributes[device.get("name")] = {}
             self.__setAttributeQuery[device.get("name")] = [""]
 
-        log.info('Custom connector %s initialization success.',self.get_name())
+        log.info('Custom connector %s initialization success.', self.get_name())
         log.info("Devices in configuration file found: %s ", '\n'.join(device for device in self.__devices))
 
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
-    
+
     def __connect_to_devices(self):
         device_config = self.__devices[self.__masterCameraName]['device_config']
         self.__gateway.add_device(device_config["name"], {"connector": self}, device_type=device_config["type"])
@@ -78,9 +77,11 @@ class NDUGateCameraConnector(Thread, Connector):
             log.exception(e)
 
     def open(self):
+        log.info('%s connecting %s:%s', self.get_name(), HOSTNAME, PORT)
         self.stopped = False
         self.socket.connect("tcp://{}:{}".format(HOSTNAME, PORT))
-        self.socket.subscribe(self.__config.get("topic", "ndugate"))
+        topic = self.__config.get("topic", "ndugate")
+        self.socket.subscribe(topic)
         self.start()
 
     def get_name(self):
@@ -89,14 +90,15 @@ class NDUGateCameraConnector(Thread, Connector):
     def is_connected(self):
         return self.__connected
 
-    def run(self):    # Main loop of thread
-        currentConfig = self.__devices[self.__masterCameraName]['device_config']
-        deviceName =  currentConfig.get('name', 'NDUGateCamera')
-        deviceType =  currentConfig.get('deviceType', 'default')
-        
+    def run(self):  # Main loop of thread
+        log.info('%s started.', self.get_name())
+        current_config = self.__devices[self.__masterCameraName]['device_config']
+        device_name = current_config.get('name', 'NDUGateCamera')
+        device_type = current_config.get('deviceType', 'default')
+
         result_dict = {
-            'deviceName': deviceName,
-            'deviceType': deviceType,
+            'deviceName': device_name,
+            'deviceType': device_type,
             'attributes': [],
             'telemetry': [],
         }
@@ -105,7 +107,7 @@ class NDUGateCameraConnector(Thread, Connector):
             while True:
                 try:
                     data_part = self.socket.recv_string()
-
+                    log.info('%s got data %s', self.get_name(), data_part)
                     if not data_part:
                         continue
 
@@ -116,35 +118,39 @@ class NDUGateCameraConnector(Thread, Connector):
                     data = json.loads(json_string)
 
                     result_dict['telemetry'] = []
-                    result_dict['telemetry'] = []
+                    result_dict['attributes'] = []
 
                     if data is None:
                         continue
 
-                    for key in data:
-                        item = {}
-                        item[key] = data[key]
-                        result_dict['telemetry'].append(item)
-                    log.debug("Data %s", result_dict)
+                    if data.get("telem") is not None:
+                        telem_data = data.get("telem")
+                        for key in data.get("telem"):
+                            item = {}
+                            item[key] = telem_data[key]
+                            result_dict['telemetry'].append(item)
+
+                    if data.get("attr") is not None:
+                        attr_data = data.get("attr")
+                        # for key in attr_data:
+                        #     item = {}
+                        #     item[key] = attr_data[key]
+                        result_dict['attributes'].append(attr_data)
+
+                    log.info("Data %s", result_dict)
                     self.__gateway.send_to_storage(self.get_name(), result_dict)
                     time.sleep(0.1)
                 except Exception as e:
-                    self.log_exception(e)
-                    time.sleep(5) # socket hatası olursa daha uzun süre uyu
+                    log_exception(e)
+                    time.sleep(5)  # socket hatası olursa daha uzun süre uyu
         except Exception as e:
-            self.log_exception(e)
-
-    def log_exception(self, e):
-        if hasattr(e, 'message'):
-            log.error(e.message)
-        else:
-            log.exception(e)
+            log_exception(e)
 
     def close(self):
         if self.context:
             self.context.destroy()
         self.stopped = True
-        self.__gateway.del_device(self.__devices[self.__masterCameraName])
+        # self.__gateway.del_device(self.__devices[self.__masterCameraName])
 
     def on_attributes_update(self, content):
         device_name = content["device"]
