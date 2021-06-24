b295607a50dc98c693ee24e72a26b1bc73b7bd7e
agtokty <agitoktay@gmail.com>
2021-06-24 17:47:42 +0300

    _get_config bug fix for unix - change default path of config file & renaming

--- thingsboard_gateway/tb_gateway.py
+++ thingsboard_gateway/tb_gateway.py
@@ -20,41 +20,41 @@ import traceback
 from sys import platform
 from thingsboard_gateway.gateway.tb_gateway_service import TBGatewayService
 
-DEFAULT_TB_GATEWAY_CONF = "/etc/ndu-gateway/config/tb_gateway.yaml"
-DEFAULT_TB_GATEWAY_CONF_WIN = "C:/ndu-gateway/config/tb_gateway.yaml"
+DEFAULT_TB_GATEWAY_CONF = "/etc/thingsboard-gateway/config/tb_gateway.yaml"
+DEFAULT_TB_GATEWAY_CONF_WIN = "C:/thingsboard-gateway/config/tb_gateway.yaml"
+
 
 def _get_config():
     print(sys.argv)
-    ndu_gateway_config_file = None
+    tb_gateway_config_file = None
 
     if len(sys.argv) > 1:
         try:
             opts, _ = getopt.getopt(sys.argv[1:], "c:", ["config="])
             for opt, arg in opts:
                 if opt in ['-c', '--config']:
-                    ndu_gateway_config_file = arg
+                    tb_gateway_config_file = arg
         except getopt.GetoptError:
             print('tb_gateway.py -c <config_file_path>')
 
-    config_file_name = "ndu_gate.yaml"
-    if ndu_gateway_config_file is None:
+    config_file_name = "tb_gateway.yaml"
+    if tb_gateway_config_file is None:
         config_file_name = "tb_gateway.yaml"
 
         config_file = path.dirname(path.abspath(__file__)) + '/config/'.replace('/', path.sep) + config_file_name
         if path.isfile(config_file):
-            ndu_gateway_config_file = config_file
+            tb_gateway_config_file = config_file
 
-    if ndu_gateway_config_file is None:
+    if tb_gateway_config_file is None:
         print("Config file not specified, going to use default")
         if platform == "win32":
-            ndu_gateway_config_file = DEFAULT_TB_GATEWAY_CONF.replace('/', path.sep)
+            tb_gateway_config_file = DEFAULT_TB_GATEWAY_CONF.replace('/', path.sep)
         else:
-            ndu_gateway_config_file = DEFAULT_TB_GATEWAY_CONF_WIN.replace('/', path.sep)
+            tb_gateway_config_file = DEFAULT_TB_GATEWAY_CONF_WIN.replace('/', path.sep)
 
-    print("Config file is {}".format(ndu_gateway_config_file))
+    print("Config file is {}".format(tb_gateway_config_file))
 
-    return ndu_gateway_config_file
-
+    return tb_gateway_config_file
 
 
 def main():
